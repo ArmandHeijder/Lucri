74b1703842874fc33535e9be8beb475875764844
TherioLZ <venomoussnake@live.com>
2018-03-23 15:03:25 +0100

    First Commit

---
+++ WindowsFormsApp2/dataGetter.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace ProjLucri
+{
+    class DataGetter
+    {
+        private SqlConnection connection;
+
+        public DataGetter(string serverIP, string serverPort, string userName, string password)
+        {
+            connection = new SqlConnection("Data Source=" + serverIP + "," + serverPort + "; Database=TestDB;Persist Security Info=True; User ID=" + userName + "; Password=" + password);
+        }
+        public DataTable GetData(string statement)
+        {
+            DataTable dataStore = new DataTable();
+            SqlCommand sqlStatement = new SqlCommand(statement, connection);
+            SqlDataAdapter dataGrabber = new SqlDataAdapter(sqlStatement);
+
+            connection.Open();
+            dataGrabber.Fill(dataStore);
+            connection.Close();
+
+            return dataStore;
+        }
+    }
+}
